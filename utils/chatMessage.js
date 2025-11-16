38b63c791078acd269a03961bd5efe6d68486cbc
VOLTAGE_LORD <officialvoltinstasnap@gmail.com>
2025-11-16 13:29:06 +0100

    Enhance formatMessage with timestamp and destructuring
    Added timestamp to message format and used destructuring for parameters.

--- utils/chatMessage.js
+++ utils/chatMessage.js
@@ -1,13 +1,15 @@
 const moment = require('moment');
 
-const formatMessage = (data) => {
-    msg = {
-        from:data.fromUser,
-        to:data.toUser,
-        message:data.msg,
+const formatMessage = ({ fromUser, toUser, msg }) => {
+    const message = {
+        from: fromUser,
+        to: toUser,
+        message: msg,
         date: moment().format("YYYY-MM-DD"),
-        time: moment().format("hh:mm a")
-    }
-    return msg;
-}
-module.exports=formatMessage;
+        time: moment().format("hh:mm a"), // 12-hour format
+        timestamp: moment().toISOString() // optional, useful for sorting
+    };
+    return message;
+};
+
+module.exports = formatMessage;
